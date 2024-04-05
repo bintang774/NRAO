0b7a6f4ac59c96d0be865c749e656e16de792f1e
bintang774 <108184157+bintang774@users.noreply.github.com>
2024-04-05 10:10:05 +0800

    Android.mk: remove vmtouch

--- ndk/Android.mk
+++ ndk/Android.mk
@@ -5,8 +5,3 @@ LOCAL_MODULE := nrao
 LOCAL_SRC_FILES := nrao.cpp
 LOCAL_CPP_FEATURES += exceptions 
 include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := vmtouch
-LOCAL_SRC_FILES := vmtouch.cpp
-include $(BUILD_EXECUTABLE)
